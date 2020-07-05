ff8fea021f26c1740c26a96ded78f050285879d1
Nuwan08 <Nuwan08@gmail.com>
2020-07-06 04:43:01 +0530

    FInal

--- PrintByJob.Designer.cs
+++ PrintByJob.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource2 = new Microsoft.Reporting.WinForms.ReportDataSource();
             this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
             this.MESDataSet = new EXO_MES_Module.MESDataSet();
             this.PROD_JobCardViewBindingSource = new System.Windows.Forms.BindingSource(this.components);
@@ -41,9 +41,9 @@
             // reportViewer1
             // 
             this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            reportDataSource1.Name = "ProdJobCardView";
-            reportDataSource1.Value = this.PROD_JobCardViewBindingSource;
-            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            reportDataSource2.Name = "ProdJobCardView";
+            reportDataSource2.Value = this.PROD_JobCardViewBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource2);
             this.reportViewer1.LocalReport.ReportEmbeddedResource = "EXO_MES_Module.Report1.rdlc";
             this.reportViewer1.Location = new System.Drawing.Point(0, 0);
             this.reportViewer1.Name = "reportViewer1";
@@ -66,10 +66,12 @@
             // 
             // PrintByJob
             // 
+            this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.reportViewer1);
+            this.MaximizeBox = false;
             this.Name = "PrintByJob";
             this.Text = "PrintByJob";
             this.Load += new System.EventHandler(this.PrintByJob_Load);
